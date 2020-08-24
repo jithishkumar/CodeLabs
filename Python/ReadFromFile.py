d18e59df3582bcab765e3b194047e65cb4e99340
Jithish Kumar G <31956869+jithishkumarg@users.noreply.github.com>
2020-08-25 00:13:08 +0530

    Create ReadFromFile.py

---
+++ Python/ReadFromFile.py
@@ -0,0 +1,21 @@
+path = 'D:\Learning\Python\Sample.txt'
+dicti = {}
+bigC = None
+bigW = None
+
+reader = open(path,'r')
+
+for lines in reader:
+    #print(lines.strip())
+    words = lines.split()
+    #print(words)
+    for word in words:
+        dicti[word] = dicti.get(word,0)+1
+print(dicti)
+
+
+for (val,key) in list(dicti.items()):
+    if bigC is None or key > bigC:
+        bigW = val
+        bigC = key 
+print(bigW, bigC)
