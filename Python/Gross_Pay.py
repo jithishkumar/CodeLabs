bbd3eb63a365373fa6e4748e39f0023a74504c8b
Jithish Kumar G <31956869+jithishkumarg@users.noreply.github.com>
2020-08-25 01:34:50 +0530

    Update Gross_Pay.py

--- Python/Gross_Pay.py
+++ Python/Gross_Pay.py
@@ -1,3 +1,6 @@
+N_Hours = 0
+Incremented_pay = 0
+
 print('Enter the Hours')
 hours = input('')
 print('Enter hourly rate')
@@ -9,5 +12,14 @@ try:
 except:
     print('Please enter a numerical value')
     
-gross_pay = hours * hourly_rate
+if hours > 40:
+    Incremented_hourly_rate = hourly_rate * 1.5
+    Incremented_pay = Incremented_hourly_rate * (hours-40)
+    N_Hours = 40
+else:
+    N_Hours = hours
+    
+Normal_gross_pay = N_Hours * hourly_rate
+gross_pay = Normal_gross_pay + Incremented_pay
+    
 print(gross_pay)
