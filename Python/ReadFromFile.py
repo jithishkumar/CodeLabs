e1c7ebb42d4c4c1d3d63e0c5cd4e3c15df2110b7
Jithish Kumar G <31956869+jithishkumarg@users.noreply.github.com>
2020-08-25 02:14:04 +0530

    Update ReadFromFile.py

--- Python/ReadFromFile.py
+++ Python/ReadFromFile.py
@@ -8,7 +8,7 @@ try:
 except:
     print("file not found in the given directory")
     quit()
-
+###For creating a dictionary and finding the most commonly used word and its occurance
 for lines in reader:
     #print(lines.strip())
     words = lines.split()
@@ -23,3 +23,26 @@ for (val,key) in list(dicti.items()):
         bigW = val
         bigC = key 
 print(bigW, bigC)
+
+
+### With method which creates instance and deletes it once completed
+#From a given file, finds the character and and retruns a list of words
+list_Conf = []
+
+with open('Text File') as f:
+    for lines in f:
+        if lines.startswith('Starting letter or word you want to find'):
+            Conf = lines.split(':')
+            list_Conf.append(float(Conf[1].strip()))
+print(list_Conf)
+
+#Sorts the list
+
+with open('D:\Learning\Python\Sample.txt') as f:
+    for lines in f:
+        for word in lines.split():
+            if word in list_Conf:
+                continue
+            list_Conf.append(word)
+print(sorted(list_Conf))
+
